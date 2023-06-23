34556c228b49a8f6533f2bf48b5c09cb3f2c4e18
hari-p02 <hari.patchigolla@uconn.edu>
2023-06-22 18:03:17 -0700

    trying to add a light mdoe and dark mode features

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import SearchBar from './components/SearchBar';
-import Card from './components/Card';
+
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleToggleClick = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('dark-mode');
+      // const postCard = document.querySelector('.post-card');
+      // postCard.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+      // const postCard = document.querySelector('.post-card');
+      // postCard.classList.remove('dark-mode');
+    }
+  }, [isDarkMode]);
+  
   return (
    <>
-    <Navbar />
+    <div className='nav-control'>
+      <Navbar />
+      <div class="toggleWrapper">
+        <input
+            type="checkbox"
+            className="dn"
+            id="dn"
+            checked={isDarkMode}
+            onClick={handleToggleClick}
+          />
+        <label for="dn" class="toggle">
+            <span class="toggle__handler">
+                <span class="crater crater--1"></span>
+                <span class="crater crater--2"></span>
+                <span class="crater crater--3"></span>
+            </span>
+            <span class="star star--1"></span>
+            <span class="star star--2"></span>
+            <span class="star star--3"></span>
+            <span class="star star--4"></span>
+            <span class="star star--5"></span>
+            <span class="star star--6"></span>
+        </label>
+      </div>
+    </div>
     <div className='search-content'>
       <div>
-        <SearchBar/>
+        <SearchBar />
         </div>
     </div>
    </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,15 @@ import React from 'react'
 import "./Navbar.css"
 const Navbar = () => {
   return (
-    <div class="container">
-        <div class="tabs">
-            <input type="radio" id="radio-1" name="tabs"/>
-            <label class="tab" for="radio-1">Search</label>
-            <input type="radio" id="radio-2" name="tabs"/>
-            <label class="tab" for="radio-2">About</label>
-            <span class="glider"></span>
-        </div>
-    </div>
+      <div class="container">
+          <div class="tabs">
+              <input type="radio" id="radio-1" name="tabs"/>
+              <label class="tab" for="radio-1">Search</label>
+              <input type="radio" id="radio-2" name="tabs"/>
+              <label class="tab" for="radio-2">About</label>
+              <span class="glider"></span>
+          </div>
+      </div>
   )
 }
 
